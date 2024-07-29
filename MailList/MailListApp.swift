193a5eec666559a77f9310c11bee499c5b43dec2
LiamHsu <s459958@gmail.com>
2024-07-29 11:37:57 +0800

    Init git

---
+++ MailList/MailListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailListApp.swift
+//  MailList
+//
+//  Created by Liam on 2024/7/29.
+//
+
+import SwiftUI
+
+@main
+struct MailListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
